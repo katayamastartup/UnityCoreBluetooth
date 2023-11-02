c1fa951cf00d944c796dba464ebbe044e72e7034
tsuyoshi.katayama <katayama@startup-technology.com>
2023-11-02 17:27:31 +0900

    テスト中のデバイスに合わせる

--- Examples/UnityExample/Assets/Scripts/SampleUser.cs
+++ Examples/UnityExample/Assets/Scripts/SampleUser.cs
@@ -4,6 +4,16 @@ using UnityEngine.UI;
 #if UNITY_EDITOR_OSX || UNITY_IOS
 using UnityCoreBluetooth;
 
+/// <summary>
+/// BLEデバイスのの設定値
+/// </summary>
+static class BleDevice
+{
+    public const string SERVICE_UUID        = "55725ac1-066c-48b5-8700-2d9fb3603c5e";
+    public const string CHARACTERISTIC_UUID = "69ddb59c-d601-4ea4-ba83-44f679a670ba";
+    public const string DEVICE_NAME         = "MyBLEDevice";
+}
+
 public class SampleUser : MonoBehaviour
 {
 
@@ -27,8 +37,8 @@ public class SampleUser : MonoBehaviour
         manager.OnDiscoverPeripheral((CoreBluetoothPeripheral peripheral) =>
         {
             if (peripheral.name != "")
-                Debug.Log("discover peripheral name: " + peripheral.name); 
-            if ((peripheral.name != "Daydream controller") && (peripheral.name != "M5Stack") && (peripheral.name != "M5StickC")) return;
+                Debug.Log("discover peripheral name: " + peripheral.name);
+            if ((peripheral.name != BleDevice.DEVICE_NAME ) && (peripheral.name != "M5Stack") && (peripheral.name != "M5StickC")) return;
 
             manager.StopScan();
             manager.ConnectToPeripheral(peripheral);
@@ -43,7 +53,7 @@ public class SampleUser : MonoBehaviour
         manager.OnDiscoverService((CoreBluetoothService service) =>
         {
             Debug.Log("discover service uuid: " + service.uuid);
-            if (service.uuid != "FE55") return;
+            if (service.uuid.ToLower() != BleDevice.SERVICE_UUID) return;
             service.discoverCharacteristics();
         });
 
@@ -66,12 +76,16 @@ public class SampleUser : MonoBehaviour
         {
             this.value = data;
             this.flag = true;
+
+            this.command = System.Text.Encoding.ASCII.GetString(data);
+            Debug.Log("update value => " + this.command);
         });
         manager.Start();
     }
 
     private bool flag = false;
     private byte[] value = new byte[20];
+    private string command = "";
 
     private float vy = 0.0f;
 
@@ -91,7 +105,8 @@ public class SampleUser : MonoBehaviour
         this.transform.Rotate(2, -3, 4);
         if (flag == false) return;
         flag = false;
-        text.text = $"Notify: {BitConverter.ToInt32(value, 0)}";
+        // text.text = $"Notify: {BitConverter.ToInt32(value, 0)}";
+        text.text = $"Notify: {this.command}";
         vy += 0.1f;
         transform.position += new Vector3(0, vy, 0);
     }
